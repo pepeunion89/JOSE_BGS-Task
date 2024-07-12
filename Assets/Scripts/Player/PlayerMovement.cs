a5aae1c0ddfdc672d1767927107c85f551a9d00f
pepeunion89 <pepeunion89@gmail.com>
2024-07-12 00:12:04 -0300

    PlayerMovement script animations fixed

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -3,98 +3,72 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{
+public class PlayerMovement : MonoBehaviour {
     [Range(1f, 5f)]
     [SerializeField] public float movementSpeed = 1.0f;
     [SerializeField] public Rigidbody2D player;
     private Vector2 direction;
-    private int flagStart = 0;
     private Animator animator;
-    public void HandleMovement(Vector2 movementDirection) {
 
+    public void HandleMovement(Vector2 movementDirection) {
         direction = movementDirection * movementSpeed;
         player.velocity = direction;
-
     }
 
     public void UpdateDirectionMovementAnimation(Vector2 movementDirection, Transform boy) {
-
-        if(flagStart == 0) {
-
+        if (animator == null) {
             animator = boy.GetComponent<Animator>();
-
-            flagStart = 1;
+            if (animator == null) {
+                Debug.LogError("Animator not found on " + boy.name);
+                return;
+            }
         }
 
-        if(flagStart == 1) {
+        ResetAllAnimationBools();
 
-            Debug.Log(Math.Truncate(direction.x * 10));
-            if (direction.x == 0 && direction.y == 5) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, true);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
-            }
-
-            if (direction.x == 5 && direction.y == 0) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, true);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
-            }
-
-            if (direction.x == 0 && direction.y == -5) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, true);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
-            }
-
-            if (direction.x == -5 && direction.y == 0) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, true);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, false);
-            }
-
-            if (Math.Truncate(direction.x * 10) == 35 && Math.Truncate(direction.y * 10) == 35) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, true);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
-            }
-
-            if (Math.Truncate(direction.x * 10) == -35 && Math.Truncate(direction.y * 10) == 35) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, true);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
-            }
-
-            if (Math.Truncate(direction.x * 10) == 35 && Math.Truncate(direction.y * 10) == -35) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, true);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
-            }
-
-            if (Math.Truncate(direction.x * 10) == -35 && Math.Truncate(direction.y * 10) == -35) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, true);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
-            }
-
-            if (direction.x == 0 && direction.y == 0) {
-                animator.SetBool(Constants.PlayerConstants.IsWalkingUp, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingDown, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
-                animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
-            }
-
+        if (movementDirection == Vector2.zero) {
+            // No movement
+            return;
         }
 
+        if (Mathf.Approximately(movementDirection.x, 0) && Mathf.Approximately(movementDirection.y, 1)) {
+
+            animator.SetBool(Constants.PlayerConstants.IsWalkingUp, true);
+
+        } else if (Mathf.Approximately(movementDirection.x, 1) && Mathf.Approximately(movementDirection.y, 0)) {
+
+            animator.SetBool(Constants.PlayerConstants.IsWalkingRight, true);
+
+        } else if (Mathf.Approximately(movementDirection.x, 0) && Mathf.Approximately(movementDirection.y, -1)) {
+
+            animator.SetBool(Constants.PlayerConstants.IsWalkingDown, true);
+
+        } else if (Mathf.Approximately(movementDirection.x, -1) && Mathf.Approximately(movementDirection.y, 0)) {
+
+            animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, true);
+
+        } else if (Mathf.Approximately(movementDirection.x, 0.707f) && Mathf.Approximately(movementDirection.y, 0.707f)) {
+
+            animator.SetBool(Constants.PlayerConstants.IsWalkingUp, true);
+
+        } else if (Mathf.Approximately(movementDirection.x, -0.707f) && Mathf.Approximately(movementDirection.y, 0.707f)) {
+
+            animator.SetBool(Constants.PlayerConstants.IsWalkingUp, true);
+
+        } else if (Mathf.Approximately(movementDirection.x, 0.707f) && Mathf.Approximately(movementDirection.y, -0.707f)) {
+
+            animator.SetBool(Constants.PlayerConstants.IsWalkingDown, true);
+
+        } else if (Mathf.Approximately(movementDirection.x, -0.707f) && Mathf.Approximately(movementDirection.y, -0.707f)) {
+
+            animator.SetBool(Constants.PlayerConstants.IsWalkingDown, true);
+        }
     }
 
+    private void ResetAllAnimationBools() {
+        animator.SetBool(Constants.PlayerConstants.IsWalkingUp, false);
+        animator.SetBool(Constants.PlayerConstants.IsWalkingDown, false);
+        animator.SetBool(Constants.PlayerConstants.IsWalkingLeft, false);
+        animator.SetBool(Constants.PlayerConstants.IsWalkingRight, false);
+    }
 }
